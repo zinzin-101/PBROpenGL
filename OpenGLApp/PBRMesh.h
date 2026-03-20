1fc7d545f55cec6c0f72d3dbb551d3cdebac83fd
zinzin-101 <96015391+zinzin-101@users.noreply.github.com>
2026-03-20 17:01:03 +0700

    bug fix

--- OpenGLApp/PBRMesh.h
+++ OpenGLApp/PBRMesh.h
@@ -147,7 +147,7 @@ public:
                 number = std::to_string(aoNrPBR++);
 
             // now set the sampler to the correct texture unit
-            glUniform1i(glGetUniformLocation(shader.ID, (name + number).c_str()), GL_TEXTURE4 + i);
+            glUniform1i(glGetUniformLocation(shader.ID, (name + number).c_str()), 4 + i);
             glUniform1i(glGetUniformLocation(shader.ID, "useMR"), (metallicNrPBR != 1 && roughnessNrPBR == 1) ? 1 : 0);
             // and finally bind the texture
             glBindTexture(GL_TEXTURE_2D, textures[i].id);
